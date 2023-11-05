626f279144dc50b26be92aa6f4d43e554a5ac352
Okazaki92 <vincentslominski@gmail.com>
2023-11-05 16:18:16 +0100

    fix

--- src/components/UserMenu/UserMenu.tsx
+++ src/components/UserMenu/UserMenu.tsx
@@ -1,9 +1,12 @@
 import { Button } from "@mui/material";
-import { useAppDispatch } from "../../hooks/hooks";
+import { useAppDispatch, useAppSelector } from "../../hooks/hooks";
 import { logout } from "../../redux/auth/authOperation";
+import { selectUser } from "../../redux/auth/authSelectors";
 
 export const UserMenu = () => {
   const dispatch = useAppDispatch();
+  useAppSelector(selectUser);
+  console.log(selectUser);
 
   return (
     <div className="wrapper">
